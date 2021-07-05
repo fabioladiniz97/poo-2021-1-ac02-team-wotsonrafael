21eb6ca9f705ff793759ff6a2c93273f15fe4742
william jefferson <49909461+williamjbf@users.noreply.github.com>
2021-07-05 21:51:30 +0000

    CBBAtividadePratica2

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,13 @@
+class CBBAtividadePratica2{
+  public static void main(String[] args){
+    int num1 = 72;
+    int num2 = 15;
+    int num3 = 10;
+
+    double soma = num1 +  num2 + num3;
+    double media = soma / 3;
+
+    System.out.println("A soma é igual a " + soma);
+    System.out.println("A média da soma é igual a " + media);
+  }
+}
