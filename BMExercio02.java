d5432b4f52000f046aad30e593f68daa8a394a5a
william jefferson <49909461+williamjbf@users.noreply.github.com>
2021-07-05 22:33:19 +0000

    BMExercio02

---
+++ BMExercio02.java
@@ -0,0 +1,20 @@
+class BMExercio02{
+  public static void main(String[] args){
+    int i = 1;
+    long j = 2;
+
+    float p = 20;
+    double q = 30.0;
+
+    boolean b = true;
+
+    char c = 'K';
+
+    System.out.println("Inteiro i de 32 bits com valor " + i);
+    System.out.println("Inteiro j de 64 bits com valor " + j);
+    System.out.println("Ponto flutuante p de 32 bits com valor " + p);
+    System.out.println("Ponto flutuante q de 64 bits com valor " + q);
+    System.out.println("Boolean b com valor " + b);
+    System.out.println("Caracter c com valor " + c);
+  }
+}
