3dad1d90434b092ee9c99cb1d639fa2aae79b6dc
william jefferson <49909461+williamjbf@users.noreply.github.com>
2021-07-05 22:01:11 +0000

    CBBAtividadePratica4

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,14 @@
+class CBBAtividadePratica4{
+  public static void main(String[] args){
+
+    int valor_cubo = 100;
+
+    float altura = 3;
+    float lateral_1 = 30;
+    float lateral_2 = 20;
+
+    float orcamento = (altura*lateral_1*lateral_2)*valor_cubo;
+    
+    System.out.println("O valor do orçamento é de R$ " + orcamento);
+  }
+}
