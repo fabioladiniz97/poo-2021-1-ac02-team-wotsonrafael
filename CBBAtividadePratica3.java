606aa2b65a96c68380d7de2693231b4d229ac950
Kauanny Vieira <72564756+Kauanny-cmd@users.noreply.github.com>
2021-07-05 21:54:54 +0000

    CBBAtividadePratica3

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,10 @@
+class CBBAtividadePratica3{
+  public static void main(String[] args){
+    int altura = 72;
+    int largura = 42;
+
+    int area = altura * largura;
+
+    System.out.println("A área é igual a " + area);
+  }
+}
